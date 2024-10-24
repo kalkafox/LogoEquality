206141d281bf3662885b7d7909846938b59457b2
Kalka <kalka2088@gmail.com>
2024-10-23 21:16:40 -0400

    1.21 commit

--- common/src/main/java/dev/kalkafox/logoequality/mixin/LogoRendererMixin.java
+++ common/src/main/java/dev/kalkafox/logoequality/mixin/LogoRendererMixin.java
@@ -2,18 +2,17 @@ package dev.kalkafox.logoequality.mixin;
 
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.LogoRenderer;
+import net.minecraft.resources.ResourceLocation;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LogoRenderer.class)
 public class LogoRendererMixin {
 
-    @Inject(method = "renderLogo(Lnet/minecraft/client/gui/GuiGraphics;IFI)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;blit(Lnet/minecraft/resources/ResourceLocation;IIFFIIII)V", shift = At.Shift.AFTER), cancellable = true)
-    private void removeEditionLogo(GuiGraphics guiGraphics, int i, float f, int j, CallbackInfo ci) {
-        ci.cancel();
-        guiGraphics.setColor(1.0F, 1.0F, 1.0F, 1.0F);
-    }
+    @Redirect(method = "renderLogo(Lnet/minecraft/client/gui/GuiGraphics;IFI)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;blit(Lnet/minecraft/resources/ResourceLocation;IIFFIIII)V", ordinal = 1))
+    private void removeEditionLogo(GuiGraphics instance, ResourceLocation resourceLocation, int i, int j, float f, float g, int k, int l, int m, int n) {}
 
 }
